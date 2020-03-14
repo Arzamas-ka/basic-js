70ac74416c4ff1afcd7140e48785808609ad8aeb
Liudmila_Arzamasova <liudmila.arzamasova@gmail.com>
2020-03-14 23:01:21 +0300

    feat: implement task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct) {
+      this.direct = direct === false ? false : true;
     }
-
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  
+    ordA(a) {
+      return a.charCodeAt(0) - 65;
     }
-}
-
-module.exports = VigenereCipheringMachine;
+  
+    encrypt(word, keyword) {
+      if (!word || !keyword) {
+        throw new Error('Incorrect word or keyword');
+      }
+  
+      const result = this.vigenere(word, keyword);
+  
+      return this.direct
+        ? result
+        : result
+            .split('')
+            .reverse()
+            .join('');
+    }
+  
+    decrypt(word, keyword) {
+      if (!word || !keyword) {
+        throw new Error('Incorrect word or keyword');
+      }
+  
+      const result = this.vigenere(word, keyword, true);
+  
+      return this.direct
+        ? result
+        : result
+            .split('')
+            .reverse()
+            .join('');
+    }
+  
+    vigenere(text, key, decode) {
+      let i = 0;
+      let b;
+      const ordA = this.ordA;
+  
+      key = key.toUpperCase();
+  
+      return text.toUpperCase().replace(/[A-Z]/g, function(a) {
+        b = key[i++ % key.length];
+        return String.fromCharCode(
+          ((ordA(a) + (decode ? 26 - ordA(b) : ordA(b))) % 26) + 65,
+        );
+      });
+    }
+  }
+  
+  module.exports = VigenereCipheringMachine;
